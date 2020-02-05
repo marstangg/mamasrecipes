0b8229e0227d866b4ee0c05d9b0d8f99bcafe882
Ubuntu <ubuntu@ip-172-31-84-48.ec2.internal>
2020-02-05 07:03:41 +0000

    template created.

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request   
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__  == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('port')),
+            debug=True)
+            
+        
