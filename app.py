1a1e3fa0cfcdb81df91075ed61d131e93677c18d
marstangg <marcussltang@gmail.com>
2020-02-16 04:16:23 +0000

    created recipes html. link to recipes on navbar is working.

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
-from flask import Flask, render_template, request   
-<<<<<<< HEAD
+from flask import Flask, render_template, request, redirect, url_for  
 import os, pymongo
-
-app = Flask(__name__)
+from bson.objectid import ObjectId
 
 MONGO_URI="mongodb+srv://root:1234@cluster0-byoef.mongodb.net/test?retryWrites=true&w=majority"
 MONGO_DB="mamarecipes"
 
+app = Flask(__name__)
+
 def get_connection():
     conn=pymongo.MongoClient(MONGO_URI)
     return conn
-
-# paul --> mongo "mongodb+srv://cluster0-byoef.mongodb.net/test"  --username root
-
-@app.route("/")
-def index():
-    conn = get_connection()
-    call_data = conn[MONGO_DB]["Western"].find()
     
-    # return render_template("index.html", call_data=call_data)
-    return render_template("index.html", call_data=call_data)
-    
-=======
-import os
-
-app = Flask(__name__)
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
->>>>>>> 276f6af3a86ef3b30ca9d3b15555430d2c33ed47
+@app.route("/recipes")
+def recipes():
+    
+    return render_template("recipes.html")
+
 if __name__  == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
