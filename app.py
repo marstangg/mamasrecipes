eb998b8705a95ecaa16db61f278795ecd957a503
marstangg <marcussltang@gmail.com>
2020-05-02 13:29:16 +0000

    completed app.py comments

--- app.py
+++ app.py
@@ -17,23 +17,23 @@ conn = pymongo.MongoClient(MONGO_URI)
 # set 'data' to be the name to represent the database link
 recipe_data = conn[DATABASE_NAME][COLLECTION_NAME]
 
-""" Display Landing Page """    
+# Display Landing Page 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 """ Show all record in database """
 
+# Display all recipes
 @app.route("/recipes")
 def recipes():
     recipes = recipe_data.find()
     return render_template("recipes.html", recipes=recipes)
 
-# """ Add a new recipe """
+# Add a new recipe
 @app.route("/recipe_add")
 def recipe_add():
     return render_template("recipe_add.html")
-    
 @app.route('/recipe_add', methods=['POST'])
 def save_recipe():
     username = request.form["username"]
@@ -54,13 +54,13 @@ def save_recipe():
     })
     return redirect(url_for('index'))
 
+# Edit a recipe
 @app.route('/recipe_edit/<recipe_id>')
 def recipe_edit (recipe_id):
     selected_recipe = recipe_data.find_one({
         "_id": ObjectId(recipe_id)
     })
     return render_template ('recipe_edit.html', recipe=selected_recipe)
-    
 @app.route("/recipe_edit/<recipe_id>", methods=['POST'])
 def recipe_update(recipe_id):
 
@@ -87,27 +87,24 @@ def recipe_update(recipe_id):
     
     return redirect(url_for("recipes"))
     
-# Delete Route 
+# Check if user confirm delete a recipe
 @app.route("/recipe_delete/<recipe_id>")
 def recipe_delete(recipe_id):
     recipe = recipe_data.find({
         "_id": ObjectId(recipe_id)
     })
     return render_template("recipe_delete.html", recipe=recipe)
-    
+# Proceed to delete a recipe    
 @app.route("/recipe_delete/<recipe_id>", methods=["POST"])
 def proceed_delete(recipe_id):
-    
     recipe_data.delete_one({
         "_id": ObjectId(recipe_id)
     })
     return redirect(url_for("recipes"))
 
+# Search for a type of recipe
 @app.route("/recipe_search/<search_tag>")
 def recipe_search(search_tag):
-    # search_recipes = recipe_data.find({
-    #   "recipe_tag", search_tag
-    # })
     if (search_tag == 'all'):
         search_recipes = recipe_data.find({})
     else :
@@ -115,33 +112,6 @@ def recipe_search(search_tag):
             "recipe_tag" : search_tag,
         })
     return render_template("recipe_search.html", recipe = search_recipes)
-    
-# @app.route("/search", methods=["POST"])
-# def submit_search():
-    
-#     conn = get_connection()
-    
-#     search_term = request.form["search-term"]
-#     search_word = request.form["search-word"]
-    
-#     if search_term == "username":
-#         search_by="username"
-#     elif search_term == "ingredients":
-#         search_by="ingredients"
-#     else:
-#         search_by="recipe_tag"
-    
-#     search_recipes = conn[MONGO_DB]["recipes"].find({
-#         search_by:{"$regex":search_word, "$options":"i"}
-#     })
-    
-    
-#     return render_template("search.html", search_recipes=search_recipes)
-
- 
-
-
-    
     
 if __name__  == '__main__':
     app.run(host=os.environ.get('IP'),
